790f0edcffccfb5b3eb5ae8ada5d76b7cae82242
LHanji <lohana100739@gmail.com>
2019-09-09 22:33:20 -0300

    register form-groups

--- register.php
+++ register.php
@@ -10,35 +10,44 @@
 </head>
 
 <body>
-    <form action="" method="POST">
-        <div class="form-group text-center">
-            <h1 class="display-4">Cadastro de Usuário</h1>
-        </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <form action="" method="POST">
+                <br>
+                <div class="form-group text-center">
+                    <h1 class="display-4">Cadastro de Usuário</h1>
+                </div>
+                <br>
 
-        <div class="row form-group">
-            <div class="col">
-                <span>Nome</span>
-                <input type="text" name="firstname" id="firsname" class="form-control">
-            </div>
-            <div class="col">
-                <span>Sobrenome</span>
-                <input type="text" name="lastname" id="lastname" class="form-control">
-            </div>
+                <div class="row form-group">
+                    <div class="col">
+                        <span>Nome:</span>
+                        <input type="text" name="firstname" id="firsname" class="form-control">
+                    </div>
+                    <div class="col">
+                        <span>Sobrenome:</span>
+                        <input type="text" name="lastname" id="lastname" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="username">Nome de Usuário:</label>
+                    <input type="text" name="username" id="username" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="pwd">Senha:</label>
+                    <input type="password" name="pwd" id="pwd" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="pwd2">Confirmar senha:</label>
+                    <input type="password" name="pwd2" id="pwd2" class="form-control">
+                </div>
+                <br>
+                <button type="submit" name="reg" class="btn btn-primary btn-md btn-block">Cadastrar-se</button>
+                <br>
+                <p>Já possui conta?<a href="login.php"> Faça login</a>.</p>
+            </form>
         </div>
-        <div class="form-group">
-            <label for="username">Nome de Usuário</label>
-            <input type="text" name="username" id="username" class="form-control">
-        </div>
-        <div class="form-group">
-            <label for="pwd">Senha</label>
-            <input type="password" name="pwd" id="pwd">
-        </div>
-        <div class="form-group">
-            <label for="pwd2">Senha</label>
-            <input type="password" name="pwd2" id="pwd2">
-        </div>
-        <button type="submit" class="btn btn-primary btn-md btn-block">Cadastrar-se</button>
-    </form>
+    </div>
 
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
