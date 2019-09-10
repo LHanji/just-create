bb694313c10285297f67ee203b7740b0d436d7f6
LHanji <lohana100739@gmail.com>
2019-09-09 22:36:06 -0300

    include php

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
+<?php include('server.php');?>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,7 +12,8 @@
 <body>
     <div class="container">
         <div class="row justify-content-center">
-            <form action="" method="POST">
+            <form action="register.php" method="POST">
+                <?php include('errors.php');?>
                 <br>
                 <div class="form-group text-center">
                     <h1 class="display-4">Cadastro de Usuário</h1>
