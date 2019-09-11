36d3c342301633851e13d8613055a37eb22d6602
LHanji <lohana100739@gmail.com>
2019-09-11 17:28:37 -0300

    registration 0.1

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-    define('SERVER','localhost');
-    define('USERNAME','root');
-    define('DATABASE_PWD','');
-    define('DATABASE_NAME','userdata');
+    $DATABASE_NAME = 'registration';
+    $DATABASE_PWD = '';
+    $USERNAME = 'root';
+    $SERVER = 'localhost';
     $c = mysqli_connect($SERVER, $USERNAME, $DATABASE_PWD, $DATABASE_NAME);
     if(mysqli_connect_errno()){
         die('Falha na conexão '.mysqli_connect_error());
@@ -14,25 +14,29 @@
     if(empty($_POST['firstname']) || empty($_POST['lastname']) || empty($_POST['username']) || empty($_POST['email']) || empty($_POST['pwd']) || empty($_POST['pwd2'])){
         die('É necessário que todos os campos sejam preenchidos para completar o cadastro.');
     }
-    if(!filter_var($_POST['email'], FILTER_VALIDETE_EMAIL)){
+    if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
         die ('Email inválido.');
     }
     if(preg_match('/[A-Za-z0-9]+/', $_POST['username'])== 0){
         die ('Nome de Usuário inválido.');
     }
-    if(strlen($_POST['pwd'])>16 || strlen($_POST['username']) < 8){
-        die('A senha deve conter no mínimo 8 e no máximo 16 caracteres.');
+    if(($_POST['pwd']) != ($_POST['pwd2'])){
+        die('As senhas estão diferentes.');
     }
-    if($stmt = $c->prepare('SELECT id, password FROM userdata WHERE username = ?')){
+    // if(strlen($_POST['pwd'])>16 || strlen($_POST['username']) < 8){
+    //     die('A senha deve conter no mínimo 8 e no máximo 16 caracteres.');
+    // }
+
+    if($stmt = $c->prepare('SELECT id, pwd FROM userdata WHERE username = ?')){
         $stmt->bind_param('s', $_POST['username']);
         $stmt->execute();
         $stmt->store_result();
         if($stmt->num_rows > 0){
             echo 'Este nome de usuário já existe, tente novamente.';
         } else {
-            if($stmt = $c->prepare('INSERT INTO userdate (username, pwd, email, firstname, lastname) VALUES (?, ?, ?, ?, ?)')){
+            if($stmt = $c->prepare('INSERT INTO userdata (username, pwd, email, firstname, lastname) VALUES (?, ?, ?, ?, ?)')){
                 $pwd = password_hash($_POST['pwd'], PASSWORD_DEFAULT);
-                $stmt->bind_param('sss', $_POST['username'], $pwd, $_POST['email']);
+                $stmt->bind_param('sssss', $_POST['username'], $pwd, $_POST['email'], $_POST['firstname'], $_POST['lastname']);
                 $stmt->execute();
                 echo 'Cadastrado com sucesso.';
             } else {
@@ -43,4 +47,3 @@
         echo 'Não foi possível completar o cadastro.';
     }
     $c->close();
-?>
--- userdata.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `users`(
-    `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `firstname` varchar(20) NOT NULL,
-    `lastname` varchar(20) NOT NULL,
-    `username` varchar(20) NOT NULL UNIQUE,
-    `email` varchar(50) NOT NULL,
-    `password` varchar(20) NOT NULL
-)
